dd917051fa4f974ba21a31138e8822d4535ddcc7
Jairo Alves <jairocreta@hotmail.com>
2018-03-31 16:25:39 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -32,6 +34,7 @@ public class CategoriaResource {
 	}
 
 	/*explicação do metodo
+	*** Metodo para incluir uma categoria via post***
 	1º => trabalha junto com Repositories/Service/Resource
 	2º =>method tem quer ser post para enviar a requisição =>method=RequestMethod.POST
 	3º =>@RequestBody Categoria obj => para enviar os dados Json no objeto
@@ -62,6 +65,7 @@ public class CategoriaResource {
 	}
 	
 	/* explicação do metodo
+	*** Metodo para buscar todas as categorias***
 	1º => Linha 2 do metodo transformar uma lista da classe Categoria para lista da classe CategoriaDTO
 	2º => list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()) : este comando... 
 	faz a interação como um for no List<Categoria> list = service.findAll() */
@@ -74,6 +78,29 @@ public class CategoriaResource {
 				.collect(Collectors.toList());	
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
+
+	/* explicação do metodo
+	*** Metodo para buscar todas as categorias***
+	1º => Linha 2 do metodo transformar uma lista da classe Categoria para lista da classe CategoriaDTO
+	2º => list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()) : este comando... 
+	faz a interação como um for no List<Categoria> list = service.findAll() */
+	
+	@RequestMapping(value="/page",method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page",defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage",defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy",defaultValue="nome") String orderBy ,
+			@RequestParam(value="direction",defaultValue="ASC") String direction
+			) {
+
+		Page<Categoria> list = service.findPage(page,linesPerPage,orderBy,direction); 
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	
 	
 
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
@@ -51,6 +54,14 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page,Integer linesPage,String orderBy,String direction){
+		@SuppressWarnings("deprecation")
+		PageRequest pageRequest = new PageRequest(page, linesPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+		
 	}
 
 
