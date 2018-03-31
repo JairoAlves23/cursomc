b69a186625e490f7a5de009725be981148b47ff6
Jairo Alves <jairocreta@hotmail.com>
2018-03-31 13:32:32 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,7 +3,6 @@ package com.nelioalves.cursomc.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.RepositoryQuery;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,17 +24,30 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
+	// explicação do metodo
+	// trabalha junto com Repositories/Service/Resource
+	// method tem quer ser post para enviar a requisição =>method=RequestMethod.POST
+	// @RequestBody Categoria obj => para enviar os dados Json no objeto
+	// objeto uri a assinatura para o envio da requisição
+	// ResponseEntity<Void> não retornar nada no metodo
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
-	    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-	    		.buildAndExpand(obj.getId()).toUri();
-	    return ResponseEntity.created(uri).build();	
-	 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+
 	}
 
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -9,29 +9,28 @@ import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) { 
-		
+
+	public Categoria find(Integer id) {
+
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		
-	}	
-	
-	public Categoria insert(Categoria obj) {		
+
+	}
+
+	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-		
-		
-	}
-	
-	
 
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+}
